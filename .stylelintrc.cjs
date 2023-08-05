cd0dfd4a3d8e3e957c691e78b10d32ac9a80b417
Vladimir Kubantsev <trihlord@gmail.com>
2023-08-05 14:22:13 +0300

    Config css modules

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,4 +1,13 @@
 /** @type {import('stylelint').Config} */
 module.exports = {
     extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
+    rules: {
+        'selector-class-pattern': [
+            '^[a-z][a-zA-Z0-9]+$',
+            {
+                message: (selector) =>
+                    `Expected class selector "${selector}" to be lowerCamelCase`,
+            },
+        ],
+    },
 }
